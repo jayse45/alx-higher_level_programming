8e3795f5cf54d026c25eb5f336cdaa6d1f105ff6
jayse45 <68971541+jayse45@users.noreply.github.com>
2021-10-12 21:01:26 +0000

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """Function that creates a class MyList that inherits from list"""
 
-class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
-    """
 
+class MyList(list):
+    """subclass inheriting from another class or object"""
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        """Public instance method: def print_sorted(self): that prints
+            the list, but sorted (ascending sort)"""
+        print(sorted(self))
