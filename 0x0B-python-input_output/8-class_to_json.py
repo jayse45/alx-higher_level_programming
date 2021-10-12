52f065a09db1abc2b13c9d9aa4ef15c3575565f2
jayse45 <ngyasi0@gmail.com>
2021-10-12 21:04:32 +0000

    new project

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that returns the dictionary description with
+simple data structure (list, dictionary, string, integer
+and boolean) for JSON serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """
+    obj is an instance of a Class
+    """
+    return(obj.__dict__)
